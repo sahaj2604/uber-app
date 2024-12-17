d906cce491bd667e49a8b0fb5b774891ae358d7a
sahaj singh <sahajabhin@gmail.com>
2024-12-17 19:06:21 +0530

    second commit

--- backend/app.js
+++ backend/app.js
@@ -3,11 +3,18 @@ dotenv.config()
 const express=require('express')
 const cors=require('cors')
 const app=express();
+const connectToDb=require('./db/db.js')
+const userRoutes=require('./routes/user.route.js');
+const cookieParser = require('cookie-parser');
 app.use(cors())
+connectToDb()
 
+//middlerware
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
-app.get("/",(_,res)=>{
-    res.send("hello world");
-})
+//routes
+app.use("/users",userRoutes)
 
 module.exports=app;
---
+++ backend/services/user.service.js
@@ -0,0 +1,15 @@
+const userModel = require("../models/user.model");
+
+module.exports.createUser=async ({firstname,lastname,email,password}) => {
+    if(!firstname || !email || !password){
+        throw new Error("all fields are required");
+    }
+    const user=await userModel.create({
+        fullname:{
+            firstname,lastname
+        },
+        email,
+        password
+    })
+    return user;
+}
